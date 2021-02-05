1db2045d4b937edcbe9d8c796315c43a75fbe26d
Paweł Łęczycki <pawel@msd.info.pl>
2021-02-05 11:20:27 +0100

    Pobieranie konfiguracji z plików

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Models;
 using Newtonsoft.Json;
 using Services.Fakers;
@@ -14,8 +15,24 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            Service = new CrudService<User>(new UserFaker() , 15);
+            //package Microsoft.Extensions.Configuration
+            var config = new ConfigurationBuilder()
+            //package Microsoft.Extensions.Configuration.FileExtensions
+            //package Microsoft.Extensions.Configuration.json
+            .AddJsonFile("Configurations/configApp.json", optional: true, reloadOnChange: true)
+            //package Microsoft.Extensions.Configuration.ini
+            .AddIniFile("Configurations/configApp.ini", optional: true, reloadOnChange: true)
+            //package NetEscapades.Configuration.yaml
+            .AddYamlFile("Configurations/configApp.yaml", optional: true, reloadOnChange: true)
+            //package Microsoft.Extensions.Configuration.xml
+            .AddXmlFile("Configurations/configApp.xml", optional: true, reloadOnChange: true)
+            .AddYamlFile("Configurations/badconfig.yaml", optional: true, reloadOnChange: true)
+            .Build();
 
+            System.Console.WriteLine($"Hello {config["Hello"]}!");
+            System.Console.WriteLine($"Hello {config["HelloJson"]}!");
+
+            Service = new CrudService<User>(new UserFaker() , 15);
             Service.Read().ToList().ForEach(x => System.Console.WriteLine( JsonConvert.SerializeObject(x) ));
         }
     }
