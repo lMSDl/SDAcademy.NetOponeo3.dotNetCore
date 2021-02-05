4d423216ba707a812a97bc492e4d7fc1b7c36f23
Paweł Łęczycki <pawel@msd.info.pl>
2021-02-05 11:53:02 +0100

    Cleaning

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,12 +12,36 @@ namespace ConsoleApp
 {
     class Program
     {
+        private static IConfigurationRoot ConfigurationRoot {get;}
+        public static ConfigApp ConfigApp {get;}
         private static ICrudService<User> Service {get; set;}
+
+        static Program()
+        {
+            ConfigurationRoot = GerConfigrationRoot();
+            ConfigApp = new ConfigApp();
+            //package Microsoft.Extensions.Configuration.Binder
+            ConfigurationRoot.Bind(ConfigApp);
+        }
 
         static void Main(string[] args)
         {
+            ConfigDemo();
+
+            Service = new CrudService<User>(new UserFaker(), ConfigApp.Faker.NumberOfGeneratedObjects);
+            Service.Read().ToList().ForEach(x => System.Console.WriteLine(JsonConvert.SerializeObject(x)));
+        }
+
+        private static void ConfigDemo()
+        {
+            System.Console.WriteLine($"{ConfigurationRoot.GetSection("Section").GetSection("Subsection")["SubsectionKey2"]} {ConfigurationRoot.GetSection("Section")["SectionKey1"]}!");
+            System.Console.WriteLine($"{ConfigApp.Section.Subsection.SubsectionKey2} {ConfigApp.Section.SectionKey1}!");
+        }
+
+        private static IConfigurationRoot GerConfigrationRoot()
+        {
             //package Microsoft.Extensions.Configuration
-            var config = new ConfigurationBuilder()
+            return new ConfigurationBuilder()
             //package Microsoft.Extensions.Configuration.FileExtensions
             //package Microsoft.Extensions.Configuration.json
             .AddJsonFile("Configurations/configApp.json", optional: true, reloadOnChange: true)
@@ -29,18 +53,6 @@ namespace ConsoleApp
             .AddXmlFile("Configurations/configApp.xml", optional: true, reloadOnChange: true)
             .AddYamlFile("Configurations/badconfig.yaml", optional: true, reloadOnChange: true)
             .Build();
-
-            var configApp = new ConfigApp();
-            //package Microsoft.Extensions.Configuration.Binder
-            config.Bind(configApp);
-
-            System.Console.WriteLine($"{config.GetSection("Section").GetSection("Subsection")["SubsectionKey2"]} {config.GetSection("Section")["SectionKey1"]}!");
-            System.Console.WriteLine($"{configApp.Section.Subsection.SubsectionKey2} {configApp.Section.SectionKey1}!");
-
-
-
-            Service = new CrudService<User>(new UserFaker(), configApp.Faker.NumberOfGeneratedObjects);
-            Service.Read().ToList().ForEach(x => System.Console.WriteLine( JsonConvert.SerializeObject(x) ));
         }
     }
 }
