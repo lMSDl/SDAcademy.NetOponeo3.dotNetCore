90ba157614065cae0c1477db59102e87a7aa1ce1
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-09 08:46:14 +0200

    Tworzenie wątków

---
+++ Mulithreading&Multitasking/MulithreadingMultitasking/ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creating();
+        }
+
+        private static void Creating()
+        {
+            var thread1 = new Thread(new ThreadStart(Work)) { Name = "T1", IsBackground = true };
+            thread1.Start();
+
+            var thread2 = new Thread(new ParameterizedThreadStart(Work)) { Name = "T2" };
+            thread2.Start(100);
+
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 500; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {i}");
+                }
+            }).Start();
+
+            //Thread.CurrentThread.Name = "Main";
+            Work(100);
+        }
+
+        private static void Work(object state)
+        {
+            for (int i = 0; i < (int)state; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} {Thread.CurrentThread.ManagedThreadId} {i}");
+            }
+        }
+
+        private static void Work()
+        {
+            Work(1000);
+        }
+    }
+}
