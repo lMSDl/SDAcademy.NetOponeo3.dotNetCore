e1174bcae8d538cae186cafd068d7fd5e1d21b87
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-09 13:34:15 +0200

    Ćwiczenie 2

---
+++ Mulithreading&Multitasking/MulithreadingMultitasking/ConsoleAppFramework/Exercise2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Exercise2
+    {
+
+        public IEnumerable<int> Generate(int from, int count)
+        {
+            throw new NotImplementedException();
+        }
+        public IAsyncResult BeginGenerate(int from, int count)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<int> EndGenerate(IAsyncResult asyncResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
