5d08c6779f99e78f1478bc7c2a743540ab22e39e
Paweł Łęczycki <pawel@msd.info.pl>
2021-04-23 09:42:03 +0200

    Matrix - Parallel, Task, Partitioner

--- Mulithreading&Multitasking/MulithreadingMultitasking/ConsoleApp/Matrix.cs
+++ Mulithreading&Multitasking/MulithreadingMultitasking/ConsoleApp/Matrix.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -24,6 +28,23 @@ namespace ConsoleApp
             stopwatch.Stop();
             Console.WriteLine($"Sequential: {stopwatch.ElapsedMilliseconds} ms");
 
+
+            stopwatch.Restart();
+            MultiplyMatricesParallel(a, b, result);
+            stopwatch.Stop();
+            Console.WriteLine($"Parallel: {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Restart();
+            MultiplyMatricesTasks(a, b, result);
+            stopwatch.Stop();
+            Console.WriteLine($"Tasks: {stopwatch.ElapsedMilliseconds} ms");
+
+
+            stopwatch.Restart();
+            MultiplyMatricesParallelPartitioner(a, b, result);
+            stopwatch.Stop();
+            Console.WriteLine($"ParallelPartitioner: {stopwatch.ElapsedMilliseconds} ms");
+
         }
 
         private static void Sequential(int[,] a, int[,] b, int[,] result)
@@ -42,6 +63,71 @@ namespace ConsoleApp
                     result[i, ii] += temp;
                 }
             }
+        }
+
+        static void MultiplyMatricesTasks(int[,] a, int[,] b, int[,] result)
+        {
+            int columnSizeA = a.GetLength(1), rowSizeA = a.GetLength(0), columnSizeB = b.GetLength(1);
+
+
+            var tasks = Enumerable.Range(0, rowSizeA).Select(i => Task.Run(() =>
+            {
+                //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                for (int j = 0; j < columnSizeB; j++)
+                {
+                    int temp = 0;
+                    for (int k = 0; k < columnSizeA; k++)
+                    {
+                        temp += a[i, k] * b[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }));
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        static void MultiplyMatricesParallel(int[,] a, int[,] b, int[,] result)
+        {
+            int columnSizeA = a.GetLength(1), rowSizeA = a.GetLength(0), columnSizeB = b.GetLength(1);
+
+            Parallel.For(0, rowSizeA, i =>
+            {
+                //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                for (int j = 0; j < columnSizeB; j++)
+                {
+                    int temp = 0;
+                    for (int k = 0; k < columnSizeA; k++)
+                    {
+                        temp += a[i, k] * b[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            });
+        }
+
+        static void MultiplyMatricesParallelPartitioner(int[,] a, int[,] b, int[,] result)
+        {
+            int columnSizeA = a.GetLength(1), rowSizeA = a.GetLength(0), columnSizeB = b.GetLength(1);
+
+            var partitioner = Partitioner.Create(0, rowSizeA);
+
+
+            Parallel.ForEach(partitioner, x =>
+            {
+                for (int i = x.Item1; i < x.Item2; i++)
+                {
+                    for (int ii = 0; ii < columnSizeB; ii++)
+                    {
+                        int temp = 0;
+                        for (int iii = 0; iii < columnSizeA; iii++)
+                        {
+                            temp = a[i, iii] * b[iii, ii];
+                        }
+                        result[i, ii] += temp;
+                    }
+                }
+            });
         }
 
         private static int[,] CreateMatrix(int columnSize, int rowSize)
